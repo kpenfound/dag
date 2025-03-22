f8c4b2e864026dec6f1a76314c5c090e30c9ced8
kpenfound <kyle@dagger.io>
2025-03-22 18:11:59 -0400

    add firecrawl module
    Signed-off-by: kpenfound <kyle@dagger.io>

---
+++ firecrawl/src/firecrawl_dag/main.py
@@ -0,0 +1,81 @@
+from typing import Annotated
+from firecrawl import FirecrawlApp
+import dagger
+from dagger import Doc, function, object_type
+
+@object_type
+class CrawlPage:
+    url: str
+    title: str
+    description: str
+    content: str
+
+@object_type
+class FirecrawlDag:
+    api_key: dagger.Secret
+
+    @function
+    async def crawl(
+        self,
+        url: Annotated[str, Doc("The URL to crawl.")],
+    ) -> list[CrawlPage]:
+        """Crawl a URL and return the content."""
+        app = FirecrawlApp(api_key=await self.api_key.plaintext())
+
+        # Crawl a website:
+        crawl_response = app.crawl_url(
+          url,
+          params={
+            'limit': 100,
+            'scrapeOptions': {'formats': ['markdown']}
+          },
+          poll_interval=30
+        )
+        pages = []
+        for page in crawl_response['data']:
+            pages.append(CrawlPage(
+                url=page['metadata']['sourceURL'],
+                title=page['metadata']['title'],
+                description=page['metadata']['description'],
+                content=page['markdown']
+            ))
+        return pages
+
+    @function
+    async def scrape(
+        self,
+        url: Annotated[str, Doc("The URL to scrape.")],
+    ) -> str:
+        """Scrape a URL and return the content."""
+        app = FirecrawlApp(api_key=await self.api_key.plaintext())
+
+        # Scrape a website:
+        scrape_result = app.scrape_url(url, params={'formats': ['markdown']})
+        return scrape_result["markdown"]
+
+    @function
+    async def map(
+        self,
+        url: Annotated[str, Doc("The URL to scrape.")],
+    ) -> list[str]:
+        """Map a website and get all the urls on the website"""
+        app = FirecrawlApp(api_key=await self.api_key.plaintext())
+
+        # Map a website:
+        map_result = app.map_url(url)
+        return map_result["links"]
+
+    @function
+    async def extract(
+        self,
+        urls: Annotated[list[str], Doc("The URLs to scrape.")],
+        prompt: Annotated[str, Doc("The description of the information to extract.")],
+    ) -> str:
+        """Extract structured data from a website"""
+        # app = FirecrawlApp(api_key=await self.api_key.plaintext())
+        # FIXME: The ExtractParams seems to be different from the docs
+        # data = app.extract(urls, {
+        #     'prompt': prompt,
+        # })
+        # return data["data"]
+        return "Not implemented"
