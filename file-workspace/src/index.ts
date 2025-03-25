fa25aa6f8cef4791f4bc87a9986323930f276b27
kpenfound <kyle@dagger.io>
2025-03-24 22:02:11 -0400

    basic file workspace
    Signed-off-by: kpenfound <kyle@dagger.io>

---
+++ file-workspace/src/index.ts
@@ -0,0 +1,37 @@
+/**
+ * A workspace for reading and writing files in a project
+ */
+import { dag, Container, Directory, object, func } from "@dagger.io/dagger";
+
+@object()
+export class FileWorkspace {
+  /**
+   * The source directory of the project
+   */
+  @func()
+  source: Directory;
+
+  constructor(source: Directory) {
+    this.source = source;
+  }
+
+  /**
+   * Read a file from the project
+   * @param path The path to the file
+   */
+  @func()
+  async read(path: string): Promise<string> {
+    return this.source.file(path).contents();
+  }
+
+  /**
+   * Write a contents to a file in the project
+   * @param path The path to the file
+   * @param content The content to write in the file
+   */
+  @func()
+  write(path: string, content: string): FileWorkspace {
+    this.source = this.source.withNewFile(path, content);
+    return this;
+  }
+}
