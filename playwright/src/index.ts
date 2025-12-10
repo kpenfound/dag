0dd9775b4ef7c17e710f6f2e0536cf91403ab664
kpenfound <kyle@dagger.io>
2025-12-10 13:32:02 -0500

    init playwright
    Signed-off-by: kpenfound <kyle@dagger.io>

---
+++ playwright/src/index.ts
@@ -0,0 +1,24 @@
+/**
+ * A generated module for playwright functions
+ */
+import { dag, argument, Directory, object, func } from "@dagger.io/dagger";
+
+@object()
+export class Playwright {
+  /**
+   * Runs the playwright tests
+   */
+  @func()
+  test(@argument({ defaultPath: "/" }) source: Directory): Promise<String> {
+    return dag
+      .container()
+      .from("node:18")
+      .withWorkdir("/src")
+      .withDirectory("/src", source)
+      .withExec(["npm", "ci"])
+      .withExec(["npx", "playwright", "install", "--with-deps"])
+      .withExec(["npx", "playwright", "test"])
+      .file("playwright-report")
+      .contents();
+  }
+}
